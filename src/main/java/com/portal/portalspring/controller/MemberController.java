9ac9404bb9391b890e89b0af0e0bdc5f2511aba1
GnuDS <blue100408@gmail.com>
2024-01-23 23:21:48 +0900

    1. 회원 등록 Controller 추가 2. 페이지 추가    - 홈    - 회원등록    - 회원목록 3. lombok 라이브러리 추가    @Slf4j 애노테이션을 사용하여 log.info 로그 사용 4. sql/ddl.sql 파일 추가

--- src/main/java/com/portal/portalspring/controller/MemberController.java
+++ src/main/java/com/portal/portalspring/controller/MemberController.java
@@ -1,9 +1,17 @@
 package com.portal.portalspring.controller;
 
+import com.portal.portalspring.domain.Member;
 import com.portal.portalspring.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 public class MemberController {
 
@@ -14,5 +22,26 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        log.info("회원 등록 완료 : {}", member.getName());
+        return "redirect:/members";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 
 }
